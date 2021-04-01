bcacea15a3aeff1cc9aad7c2c83907f0f0c774cc
Hasan Mahmud <hasan019165@gmail.com>
2021-04-01 17:02:49 +0600

    design has been completet

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,6 @@ $(document).ready(function(){
 
 
 
-
-
 // main div margin minus
 $('.sidebarIconToggle').click(function(){
     $('#sidebarMenu').toggleClass('wellcome');
@@ -15,6 +13,25 @@ $('.sidebarIconToggle').click(function(){
 $('.cartBox').click(function(){
     $('#right-side-cart').toggleClass('wellcomeRight');
 });
+
+
+//Active product
+
+
+$('.single-product .when-cart-hover').click(function(){
+	if ( 0<num ) {
+		$(this).addClass('proActive');
+	}else{
+		$(this).removeClass('proActive');
+	}
+  
+});
+
+
+
+
+
+
 
 
 
@@ -33,10 +50,6 @@ $('.serv-btn').click(function(){
 $('#sidebarMenu ul li').click(function(){
 	$(this).addClass("active").siblings().removeClass("active");
 });
-
-
-
-
 
 
 
